11eebb143aae32b716b9cd3e6d1ef72578dbda2a
Ali <crazyanimator4l@gmail.com>
2025-05-20 02:37:37 +0200

    redeploy

--- routes/QARoute.js
+++ routes/QARoute.js
@@ -1,20 +1,26 @@
-import finalising from "../prompt_processing/second_prompt.js"
-import refining from "../prompt_processing/first_prompt.js";
 import express from "express";
+import multer from "multer";
+import fetch from "node-fetch";
+import finalising from "../prompt_processing/second_prompt.js";
+import refining from "../prompt_processing/first_prompt.js";
 
 const router = express.Router();
-const API_KEY = process.env.VISION_API
+const API_KEY = process.env.VISION_API;
 
-router.post("/answer", async (req, res) => {
- const { images } = req.body;
+const storage = multer.memoryStorage();  
+const upload = multer({ storage: storage }); 
 
-  if (!images || !Array.isArray(images) || images.length === 0) {
+router.post("/answer", upload.array('images'), async (req, res) => {
+  if (!req.files || req.files.length === 0) {
     return res.status(400).json({ message: "No images provided." });
   }
-  console.log(images)
+
   try {
+
     const ocrResults = await Promise.all(
-      images.map(async (base64) => {
+      req.files.map(async (file) => {
+        const base64 = file.buffer.toString('base64');
+
         const response = await fetch(
           `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`,
           {
@@ -51,7 +57,5 @@ router.post("/answer", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
-
-
 
 export default router;
--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const PORT = process.env.PORT;
 
 job.start();
 
-// CORS setup
 app.use(
   cors({
     origin: "http://localhost:8081",
@@ -20,15 +19,10 @@ app.use(
   })
 );
 
-// Increase the body size limit to 10MB (adjust as needed)
-app.use(express.json({ limit: "10mb" })); // For JSON payloads
-app.use(express.urlencoded({ limit: "10mb", extended: true })); // For URL-encoded data
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/processing", QARoute);
 
-// Start the server
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
   connectDB();

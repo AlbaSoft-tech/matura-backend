4286cb20648471d947a9b7118910938118b47d90
Ali <crazyanimator4l@gmail.com>
2025-05-20 19:23:14 +0200

    making body a string

--- routes/QARoute.js
+++ routes/QARoute.js
@@ -9,6 +9,8 @@ const API_KEY = process.env.VISION_API;
 router.post("/answer", async (req, res) => {
   const prompt = req.body;
 
+  console.log(prompt)
+
   try {
     if (!prompt.trim()) {
       return res.status(400).json({ message: "No prompt!" });

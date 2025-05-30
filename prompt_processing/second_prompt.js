e814611f9b3962d1dab1b186be3c10c0c632cfc9
Ali <crazyanimator4l@gmail.com>
2025-05-30 22:01:46 +0200

    final

--- prompt_processing/second_prompt.js
+++ prompt_processing/second_prompt.js
@@ -12,7 +12,7 @@ async function finalising(input) {
     apiKey: process.env.GEMINI_API,
   });
   const prompt =
-    "you will be given a text(optionaly), some extra information and question/s. All the answers that can be answered purely from the text, answer them from the text. If you are not 100% sure you can answer from the extra information, and if you are not 100% sure from there too, find as much information on the internet and answer the left overs. Answer each question clearly in albanian in full sentences without saying things like 'based on the text' or referencing the source." +
+    "you will be given a text(optionaly), some extra information and question/s. All the answers that can be answered purely from the text, answer them from the text. If you are not 100% sure you can answer from the extra information, and if you are not 100% sure from there too, find as much information on the internet and answer the left overs. Add an index before each question and answer each one clearly in albanian in full sentences without saying things like 'based on the text' or referencing the source. each answer should be put on a new line" +
     "\n";
   const first_prompt = input;
   const topic = first_prompt.nameOfLiteraryWork;

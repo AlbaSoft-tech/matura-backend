d5b21d031a3e386376c0f8557715d2785411bdc2
sk1b1d1t01let <crazyanimator4l@gmail.com>
2025-07-16 22:17:54 +0200

    changed ai model to 2.5 flash not preview

--- prompt_processing/second_prompt.js
+++ prompt_processing/second_prompt.js
@@ -86,7 +86,7 @@ async function finalising(input, language) {
     destructured;
 
   const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash-preview-04-17",
+    model: "gemini-2.5-flash",
     contents: finalPrompt,
   });
 

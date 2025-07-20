eea22a3cbc59ce244f158c4ef9871a5a1bc27360
sk1b1d1t01let <crazyanimator4l@gmail.com>
2025-07-20 21:27:45 +0200

    mut

--- routes/QARoute.js
+++ routes/QARoute.js
@@ -175,7 +175,7 @@ router.post("/compare-answers", async (req, res) => {
     const answer = await CompareAnswers(prompt);
     console.log(answer);
     user.checkTestTokens -= 1;
-    user.completedTests[language.toLowerCase()].push(index);
+    user.completedTests[langKey] = [...user.completedTests[langKey], index];
     await user.save();
     res.status(201).json({ answer });
   } catch (error) {

12fccf4aca41909b21defea5ec02c6f8332ed16d
Albasoft-tech <albasoft.tech@gmail.com>
2025-08-21 16:01:49 +0200

    mutikaka

--- prompt_processing/second_prompt.js
+++ prompt_processing/second_prompt.js
@@ -14,7 +14,7 @@ async function finalising(input, language) {
   const prompt =
     "you will be given a text(optionaly), some extra information and question/s. All the answers that can be answered purely from the text, answer them from the text. If you are not 100% sure you can answer from the extra information, and if you are not 100% sure from there too, find as much information on the internet and answer the left overs. Add an index before each question and answer them concisely. Do not leave out important information, but keep each answer as short as possible.Answer them in " +
     language +
-    " in full sentences without saying things like 'based on the text' or referencing the source. at the end of each answer add \\n" +
+    " in full sentences without saying things like 'based on the text' or referencing the source. Add each answer on a new line." +
     "\n";
   const first_prompt = input;
   const topic = first_prompt.nameOfLiteraryWork;

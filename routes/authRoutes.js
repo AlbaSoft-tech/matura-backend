bb3ef931f7235ba7bbc8cf6b69d44151d95ddcfc
Albasoft-tech <albasoft.tech@gmail.com>
2025-08-26 03:47:36 +0200

    mut

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -400,7 +400,6 @@ router.post("/changePassword", async (req, res) => {
 
 router.post("/delete", async (req, res) => {
   try {
-    const {password} = req.body
     console.log("deleting account")
     const authHeader = req.headers["authorization"];
     if (!authHeader) {
@@ -416,11 +415,8 @@ router.post("/delete", async (req, res) => {
 if (!user) {
   return res.status(404).json({ message: "User not found" });
 }
- const isPasswordCorrect = await user.comparePassword(password);
-    if (!isPasswordCorrect)
-      return res.status(400).json({ message: "Invalid credentials" });
 
-    
+
 await user.deleteOne();
 
 return res.status(200).json({ message: "User deleted successfully" });

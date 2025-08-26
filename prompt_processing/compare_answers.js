bc1dcde219e8670b913a6b1e91662ab1c63f577d
Albasoft-tech <albasoft.tech@gmail.com>
2025-08-26 02:22:16 +0200

    changed model

--- prompt_processing/compare_answers.js
+++ prompt_processing/compare_answers.js
@@ -8,7 +8,7 @@ async function CompareAnswers(input) {
   });
 
   const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
+    model: "gemini-2.5-flash",
     contents: prompt,
   });
 

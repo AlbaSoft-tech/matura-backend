9a8c52cf614e09727b4fe996ae0a49b01f4cf483
Albasoft-tech <albasoft.tech@gmail.com>
2025-08-31 02:47:36 +0200

    met

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -665,5 +665,27 @@ router.post("/verifyNewAccount", async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 });
+router.post("/deleteLink", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    await user.deleteOne();
+
+    return res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.log("Error in delete route", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 export default router;

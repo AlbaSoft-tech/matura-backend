47e3458eda371b5e148f9b998bea5a3f58c4389b
Albasoft-tech <albasoft.tech@gmail.com>
2025-10-01 23:45:09 +0200

    met

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -102,6 +102,9 @@ router.post("/signup", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL,
         pass: process.env.PASSWORD,

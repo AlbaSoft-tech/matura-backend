75fd9197e3210d334119653ee0079457d4198555
Albasoft-tech <albasoft.tech@gmail.com>
2025-11-18 03:56:35 +0100

    met

--- prompt_processing/compare_answers.js
+++ prompt_processing/compare_answers.js
@@ -1,10 +1,20 @@
 import { GoogleGenAI } from "@google/genai";
 import JSON5 from "json5";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Load .env from parent folder
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 async function CompareAnswers(input) {
   const prompt = input;
   const ai = new GoogleGenAI({
-    apiKey: "AIzaSyDp7tmz_51cVkCNW0dh3ey3KBAxwCGPB8M",
+    apiKey: process.env.GEMINI_API,
   });
 
   const response = await ai.models.generateContent({
